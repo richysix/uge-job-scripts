f878d153f2ad99259a4a9d4997af0b72ef071916
Richard White <richard.white@qmul.ac.uk>
2023-07-26 12:01:34 +0100

    Create script to sync directories to Sharepoint using rclone
    Syncs a directory and its contents to a Sharepoint directory of the
    same name. It is done using the sync command, so any files in the
    source directory that do not exist in the target Sharepoint directory
    will be deleted.
    Use copy if you want to update an archive

---
+++ dir-sync.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# dir-sync.sh - Script to sync a directory to Sharepoint
+#$ -cwd
+#$ -pe smp 1
+#$ -l h_rt=240:0:0
+#$ -l h_vmem=1G
+#$ -o sync.o
+#$ -e sync.e
+
+module load rclone/1.62.2
+
+dir=$1
+echo "Starting $drive sync 1" 1>&2
+rclone sync $dir sharepoint-qmul:$dir/ --include "*.{doc,docx,xls,xlsx,xlsm,ppt,pptx,html,png}" --ignore-size --ignore-checksum --drive-acknowledge-abuse
+echo "Starting $dir check 1" 1>&2
+rclone check $dir sharepoint-qmul:$dir/ --include "*.{doc,docx,xls,xlsx,xlsm,ppt,pptx,html,png}" --ignore-size --ignore-checksum --drive-acknowledge-abuse
+echo "Starting $dir sync 2" 1>&2
+rclone sync $dir sharepoint-qmul:$dir/ --exclude "*.{doc,docx,xls,xlsx,xlsm,ppt,pptx,html,png}" --drive-acknowledge-abuse
+echo "Starting $dir check 2" 1>&2
+rclone check $dir sharepoint-qmul:$dir/ --exclude "*.{doc,docx,xls,xlsx,xlsm,ppt,pptx,html,png}" --drive-acknowledge-abuse
+
+module unload rclone/1.62.2
+
+# AUTHOR
+#
+# Richard White <rich@buschlab.org>
+#
+# COPYRIGHT AND LICENSE
+#
+# This software is Copyright (c) 2023. Queen Mary University of London.
+#
+# This is free software, licensed under:
+#
+#  The GNU General Public License, Version 3, June 2007
