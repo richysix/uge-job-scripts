f5ed01c589cd9d1dcdffb769faefc1039397e8d8
Richard White <richard.white@qmul.ac.uk>
2023-09-21 09:36:46 +0100

    rscript-array.sh: Add option to specify version of R

--- rscript-array.sh
+++ rscript-array.sh
@@ -11,12 +11,45 @@
 
 source bash_functions.sh
 
-module load R/4.2.1
+USAGE="rscript-array.sh [options] file job_name"
+
+OPTIONS="Options:
+    -r    R version
+    -d    print debugging info
+    -v    verbose output
+    -q    turn verbose output off
+    -h    print help info"
+
+# default values
+debug=0
+verbose=1
+R_VERSION=4.2.1
+while getopts ":r:dhqv" opt; do
+  case $opt in
+    r)  R_VERSION=$OPTARG  ;;
+    d)  debug=1  ;;
+    h)  usage "" ;;
+    v)  verbose=1 ;;
+    q)  verbose=0 ;;
+    \?) usage "Invalid option: -$OPTARG" ;;
+    :)  usage "Option -$OPTARG requires an argument!" ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+R_VERSION=$( echo $R_VERSION | sed -e 's|^R/||' )
+module load R/$R_VERSION
 
 if [[ -z $2 ]]; then
     JOB=${JOB_NAME}
 else
     JOB=$2
+fi
+
+if [[ $debug -gt 0 ]]; then
+    echo "R version = $R_VERSION
+Input file: $1
+Job name: $JOB"
 fi
 
 line=`sed "${SGE_TASK_ID}q;d" $1`
